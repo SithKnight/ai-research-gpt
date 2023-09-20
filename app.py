5a92e9b223314bb10f63368804031c3ce930d4c0
SithKnight~ <kingsleyobi0011@gmail.com>
2023-09-21 00:41:16 +0100

    added some more code

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ from typing import Type
 from bs4 import BeautifulSoup
 import requests
 import json
+import streamlit as st
 from langchain.schema import SystemMessage
-from fastapi import FastAPI
+#from fastapi import FastAPI
 
 load_dotenv()
 brwoserless_api_key = os.getenv("BROWSERLESS_API_KEY")
@@ -172,35 +173,35 @@ agent = initialize_agent(
 
 
 # 4. Use streamlit to create a web app
-# def main():
-#     st.set_page_config(page_title="AI research agent", page_icon=":bird:")
+def main():
+     st.set_page_config(page_title="AI research agent", page_icon=":bird:")
 
-#     st.header("AI research agent :bird:")
-#     query = st.text_input("Research goal")
+     st.header("AI research agent :bird:")
+     query = st.text_input("Research goal")
 
-#     if query:
-#         st.write("Doing research for ", query)
+     if query:
+         st.write("Doing research for ", query)
 
-#         result = agent({"input": query})
+         result = agent({"input": query})
 
-#         st.info(result['output'])
+         st.info(result['output'])
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+     main()
 
 
 # 5. Set this as an API endpoint via FastAPI
-app = FastAPI()
+# app = FastAPI()
 
 
-class Query(BaseModel):
-    query: str
+# class Query(BaseModel):
+#    query: str
 
 
-@app.post("/")
-def researchAgent(query: Query):
-    query = query.query
-    content = agent({"input": query})
-    actual_content = content['output']
-    return actual_content
+# @app.post("/")
+# def researchAgent(query: Query):
+#    query = query.query
+#    content = agent({"input": query})
+#    actual_content = content['output']
+#    return actual_content
